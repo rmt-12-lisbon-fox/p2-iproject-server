a808914fcad4c9fff885cbf93fb22419cf51e74e
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-06-29 01:52:46 +0700

    HEROKU initialization 29 01.52

---
+++ routers/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+const AuthController = require('../controllers/authController')
+const Controller = require('../controllers/controller')
+const authentification = require('../middlewares/authentification')
+const errorHandling = require('../middlewares/errorhandling')
+const imageKit = require('../middlewares/imageKit')
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const path = require('path')
+const upload = multer({ 
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    var ext = path.extname(file.originalname);
+    if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+      return cb(new Error('Only images(png,jpg,jpeg) are allowed'))
+    }
+    cb(null, true)
+  },
+  limits:{ fileSize: 255000 }
+})
+
+router.post('/register', AuthController.register)
+router.post('/login', AuthController.login)
+router.post('/googlelogin', AuthController.googlelogin)
+
+
+router.use(errorHandling)
+
+module.exports = router
