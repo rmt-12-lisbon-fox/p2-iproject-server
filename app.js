858d0a67b0a9e54965a47adadef571915e65c129
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-06-29 04:48:31 +0700

    ok

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-  require('dotenv').config()
-}
+// if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+//   require('dotenv').config()
+// }
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
