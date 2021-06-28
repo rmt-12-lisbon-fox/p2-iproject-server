594fa91748ad0c4d39732b025dd0e6b201d37c27
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-06-29 06:39:28 +0700

    Revert "02"
    This reverts commit eb86f0485ca5bd321871467563f1bdfe157ea087.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
 }
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 const router = require('./routers')
 const cors = require('cors')
 
@@ -12,8 +12,8 @@ app.use(express.json())
 app.use(cors())
 app.use(router)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})  
+// app.listen(port, () => {
+  // console.log(`Example app listening at http://localhost:${port}`)
+// })  
 
 module.exports = app
