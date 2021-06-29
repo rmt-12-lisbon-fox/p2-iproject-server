a77df496e99f520495d98f7959e64dc07ff5bbc3
tiansyahmsalman <tiansyahisal@gmail.com>
2021-06-29 23:15:27 +0700

    test postman

--- controller/ContentController.js
+++ controller/ContentController.js
@@ -1,8 +1,37 @@
-const {Community, MyCommunity} = require('../models');
+const {Community, MyCommunity, User} = require('../models');
 
 class Controller {
     static allCommunity(req,res) {
         Community.findAll()
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(500).json({message: err.message})
+        })
+    }
+
+    static addMyCommunity(req,res) {
+        let communityId = req.params.id
+        MyCommunity.create({
+            UserId: req.user.id,
+            CommunityId: communityId
+        })
+        .then(data => {
+            res.status(200).json({message: "Success join", data})
+        })
+        .catch(err => {
+            res.status(500).json({message: err.message})
+        })
+    }
+
+    static allMyCommunity(req,res) {
+        MyCommunity.findAll({
+            where: {
+                UserId: req.user.id
+            },
+            include: [User, Community]
+        })
         .then(data => {
             res.status(200).json(data)
         })
