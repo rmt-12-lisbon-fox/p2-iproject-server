32d7acf3ecf18812eea0f9233d433bdd74ad1417
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-06-30 00:11:16 +0700

    server update 3 api 2 api 1 webhook DONE

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,7 @@
 const {Signal} = require('../models')
+const axios = require('axios')
+const { Sequelize, Op } = require('sequelize')
+const { QueryTypes } = require('sequelize');
 
 class Controller {
   static signal(req, res, next) {
@@ -16,6 +19,75 @@ class Controller {
   }
   static hello(req, res, next) {
     res.status(200).json({message: 'welcome to smartinvestment'})
+  }
+  static market(req, res, next) {
+    axios({
+      method: 'get',
+      url: 'https://indodax.com/api/tickers',
+    })
+    .then(({data}) => {
+      res.status(200).json({
+        btc: data.tickers.btc_idr.last,
+        eth: data.tickers.eth_idr.last,
+        doge: data.tickers.doge_idr.last,
+        ltc: data.tickers.ltc_idr.last,
+        xrp: data.tickers.xrp_idr.last
+      })
+    })
+    .catch(_ => { next({code:500}) })
+  }
+  static adzan(req, res, next) {
+    axios({
+      method: 'get',
+      url: 'http://api.aladhan.com/v1/timings/29-06-2021?latitude=-6.344785&longitude=106.641315&method=99&methodSettings=20.0,null,18.0&tune=0,0,-1,0,0,1,1,0,0'
+    })
+    .then(({data}) => {
+      console.log(data)
+      res.status(200).json({
+        subuh: data.data.timings.Fajr,
+        terbit: data.data.timings.Sunrise,
+        duhur: data.data.timings.Dhuhr,
+        asar: data.data.timings.Asr,
+        magrib: data.data.timings.Maghrib,
+        isya: data.data.timings.Isha
+      })
+    })
+    .catch(_ => { next({code:500}) })
+  }
+  static bestxrp(req, res, next) {
+    // Signal.count({
+    //   attributes: ['title', 'signal', 'createdAt'],
+    //   group: ['title', 'signal', 'createdAt'],
+    //   order: [ Sequelize.fn('MAX', Sequelize.col('createdAt'))]
+    // })
+    Signal.findAll({ where: { title: 'XRPIDR' }, order: [['createdAt', 'desc']], limit: 1 })
+    .then(([instance]) => {
+      res.status(200).json({xrp: instance.signal})
+    }).catch(_ => { next({code:500}) })
+  }
+  static bestbtc(req, res, next) {
+    Signal.findAll({ where: { title: 'BTCIDR' }, order: [['createdAt', 'desc']], limit: 1 })
+    .then(([instance]) => {
+      res.status(200).json({btc: instance.signal})
+    }).catch(_ => { next({code:500}) })
+  }
+  static besteth(req, res, next) {
+    Signal.findAll({ where: { title: 'ETHIDR' }, order: [['createdAt', 'desc']], limit: 1 })
+    .then(([instance]) => {
+      res.status(200).json({eth: instance.signal})
+    }).catch(_ => { next({code:500}) })
+  }
+  static bestltc(req, res, next) {
+    Signal.findAll({ where: { title: 'LTCIDR' }, order: [['createdAt', 'desc']], limit: 1 })
+    .then(([instance]) => {
+      res.status(200).json({ltc: instance.signal})
+    }).catch(_ => { next({code:500}) })
+  }
+  static bestdoge(req, res, next) {
+    Signal.findAll({ where: { title: 'DOGEIDR' }, order: [['createdAt', 'desc']], limit: 1 })
+    .then(([instance]) => {
+      res.status(200).json({doge: instance.signal})
+    }).catch(_ => { next({code:500}) })
   }
 }
 
