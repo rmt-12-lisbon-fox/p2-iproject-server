120fc4444f114185fa856091b97cc69321eb4ee5
Galuh Alifani <galuh.adika@gmail.com>
2021-07-01 02:20:27 +0700

    deployment port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const router = require('./routes/index.js')
 var cors = require('cors')
 
