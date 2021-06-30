866579ce5d59e32d7d1f7a3a44419efe5cd15d40
Senna Dwi Prasetyo <sennaprasetyo@yahoo.co.id>
2021-07-01 01:45:07 +0700

    Done, minus deploy

--- controllers/designController.js
+++ controllers/designController.js
@@ -1,4 +1,5 @@
 const { Design, Category, User } = require('../models')
+const nodemailer = require('nodemailer')
 
 class Controller {
     static findAll(req, res){
@@ -169,6 +170,61 @@ class Controller {
                 res.status(500).json({ message: err.message })
             }
         })
+    }
+    static sendEmail(req, res){
+        const input = {
+            from: req.body.from,
+            to: req.body.to,
+            password: req.body.password,
+            subject: req.body.subject,
+            content: req.body.content
+        }
+
+        const transporter = nodemailer.createTransport({
+            service: 'hotmail',
+            auth: {
+                user: input.from,
+                pass: input.password
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        })
+
+        const options = {
+            from: input.from,
+            to: input.to,
+            subject: input.subject,
+            text: input.content
+        }
+
+        transporter.sendMail(options, (err, info) => {
+            if (err) {
+                res.status(500).json({ message: 'Wrong email or password' })
+                return
+            }
+            else {
+                res.status(200).json({ message: 'Email sent!' })
+            }
+        })
+    }
+    static sendSms(req, res){
+        const accountSid = process.env.ACCOUNT_SID
+        const authToken = process.env.AUTH_TOKEN
+        const client = require('twilio')(accountSid, authToken)
+
+        client.messages
+            .create({
+               body: req.body.content,
+               from: '+15093090947',
+               to: '+6281222110035'
+             })
+             .then(() => {
+                res.status(200).json({ message: 'Message sent!' }) 
+             })
+            .catch(() => {
+                res.status(500).json({ message: 'SMS send unsuccessful' })
+            })
     }
 }
 
