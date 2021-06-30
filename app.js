aa61863f7a86595749a8a090e47f5a3adacf29f6
aamuzakii <aamuzakii@gmail.com>
2021-07-01 03:04:25 +0700

    config heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const controller = require('./controllers/controller')
 const userController = require('./controllers/userController')
 const cors = require('cors')
