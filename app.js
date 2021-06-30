4f709385f17302d4ccc4394e3b14f9648ba24579
tiansyahmsalman <tiansyahisal@gmail.com>
2021-07-01 03:58:35 +0700

    on deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// if(process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+if(process.env.NODE_ENV === "development") {
+  require('dotenv').config();
+}
 
-require('dotenv').config();
 const express = require('express');
 const port = process.env.PORT || 3000 
 const app = express()
