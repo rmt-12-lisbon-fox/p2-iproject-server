7631f984111ba46d731e9cb87c17777a76f1f203
Senna Dwi Prasetyo <sennaprasetyo@yahoo.co.id>
2021-07-01 02:17:30 +0700

    Heroku deploy

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
-require('dotenv').config()
+if (process.env.NODE_ENV === 'development'){
+    require('dotenv').config()
+}
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const httpServer = require('http').createServer(app)
-const io = require('socket.io')(httpServer)
 const router = require('./routes/index')
-const PORT = 3000
-const users = []
+const PORT = process.env.PORT || 3000 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(router)
-io.on('connection', (socket) => {
-
-    socket.on('sendMessage', (data) => {
-
-        io.emit('broadcastMessage', data)
-    })
-    socket.on('loginUser', (user) => {
-        users.push(user)
-        io.emit('sendUser', users)
-    })
-})
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`)
 })
