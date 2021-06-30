87fccabb84076aaf0d5869311903d4f6be11017e
Ahmad Faisal <achmad_fsl@yahoo.com>
2021-07-01 01:27:16 +0700

    add-axios

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const loginRouter = require("../routes/loginRouter");
+const registerRouter = require("../routes/registerRouter");
+const loginGoogle = require("../routes/loginGoogle");
+const challengeRoute = require("../routes/challengeRoute");
+const gameRoute = require("../routes/gameRoute");
+
+const { authentication } = require("../middlewares/auth");
+const errorHandler = require("../middlewares/errorHandler");
+
+router.use("/login", loginRouter);
+router.use("/register", registerRouter);
+router.use("/loginGoogle", loginGoogle);
+
+router.use(authentication);
+
+router.use("/games", gameRoute);
+router.use("/challenges", challengeRoute);
+router.use("/challenges", challengeRoute);
+
+module.exports = router;
+
+router.use(errorHandler);
