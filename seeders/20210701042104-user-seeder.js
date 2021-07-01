22d9e60911aa96766a990eadf6dd2b6452b631e8
nwrsadat <anwarsadat.juni@gmail.com>
2021-07-01 11:42:17 +0700

    Anwar Deploy

---
+++ seeders/20210701042104-user-seeder.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'admin@mail.com',
+        password: 'admin'
+      },
+      {
+        email: 'anwar@mail.com',
+        password: 'anwar'
+      }
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('Users')
+  }
+};
---
+++ seeders/20210701042115-comedian-seeder.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('Comedians', [
+      {
+        name: 'Greer Barnes',
+        country: 'U.S.',
+        imgUrl: 'http://www.standupny.com/wp-content/uploads/2015/10/95-1000x550.jpg',
+        ytUrl: 'https://www.youtube.com/embed/-6joN5Wz3lk',
+        birthDate: '1964-01-01'
+      },
+      {
+        name: 'Louis C.K.',
+        country: 'U.S.',
+        imgUrl: 'https://vanyaland.com/wp-content/uploads/2016/07/Louis-ck.jpg',
+        ytUrl: 'https://www.youtube.com/embed/X0IV_ZB9CDs',
+        birthDate: '1967-09-12'
+      }
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
