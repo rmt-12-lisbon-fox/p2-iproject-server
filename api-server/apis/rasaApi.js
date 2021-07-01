88b9543c16a68c363a0996309c69c751b0f4c2d3
jeffsabarman <jeffsabarman@gmail.com>
2021-07-01 19:26:27 +0700

    redeploy server

--- api-server/apis/rasaApi.js
+++ api-server/apis/rasaApi.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 
 const instance = axios.create({
-  baseURL: 'https://deco-model-server.herokuapp.com/webhooks/rest'
+  // baseURL: 'https://deco-model-server.herokuapp.com/webhooks/rest'
+  baseURL: 'http://localhost:5005/webhooks/rest'
 })
 
 module.exports = instance
